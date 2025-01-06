898956d409bbc246f901114c7b1b04375483359f
Boppana Devi Pravallika <142131479+2200030991-pravallika@users.noreply.github.com>
2025-01-06 21:04:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('#game-board');
+    const startButton = document.getElementById('start-game');
+    let cardsChosen = [];
+    let cardsChosenId = [];
+    let cardsWon = [];
+
+    const cardArray = [
+        { name: 'ec2', img: 'aws-ec2.png' },
+        { name: 'ec2', img: 'aws-ec2.png' },
+        { name: 's3', img: 'aws-s3.png' },
+        { name: 's3', img: 'aws-s3.png' },
+        { name: 'lambda', img: 'aws-lambda.png' },
+        { name: 'lambda', img: 'aws-lambda.png' },
+        { name: 'rds', img: 'aws-rds.png' },
+        { name: 'rds', img: 'aws-rds.png' },
+        { name: 'cloudfront', img: 'aws-cloudfront.png' },
+        { name: 'cloudfront', img: 'aws-cloudfront.png' },
+    ];
+    
+    function shuffle(array) {
+        array.sort(() => 0.5 - Math.random());
+    }
+
+    function createBoard() {
+        shuffle(cardArray);
+        grid.innerHTML = '';
+        cardsWon = [];
+
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img');
+            card.setAttribute('src', 'blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    function flipCard() {
+        let cardId = this.getAttribute('data-id');
+        if (!cardsChosenId.includes(cardId)) {
+            cardsChosen.push(cardArray[cardId].name);
+            cardsChosenId.push(cardId);
+            this.setAttribute('src', cardArray[cardId].img);
+            if (cardsChosen.length === 2) {
+                setTimeout(checkForMatch, 500);
+            }
+        }
+    }
+
+    function checkForMatch() {
+        const cards = document.querySelectorAll('#game-board img');
+        const firstCardId = cardsChosenId[0];
+        const secondCardId = cardsChosenId[1];
+
+        if (cardsChosen[0] === cardsChosen[1] && firstCardId !== secondCardId) {
+            cards[firstCardId].style.visibility = 'hidden';
+            cards[secondCardId].style.visibility = 'hidden';
+            cards[firstCardId].removeEventListener('click', flipCard);
+            cards[secondCardId].removeEventListener('click', flipCard);
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[firstCardId].setAttribute('src', 'blank.png');
+            cards[secondCardId].setAttribute('src', 'blank.png');
+        }
+
+        cardsChosen = [];
+        cardsChosenId = [];
+
+        if (cardsWon.length === cardArray.length / 2) {
+            alert('Congratulations! You found them all!');
+        }
+    }
+
+    startButton.addEventListener('click', createBoard);
+});
